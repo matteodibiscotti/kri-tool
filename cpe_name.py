3f0d59948a922a7041c49478d92c1bc0e42fec50
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-02-22 16:41:25 +0900

    Completed CPE name retrieval function

--- cpe_name.py
+++ cpe_name.py
@@ -5,24 +5,37 @@ import json
 PRODUCTS_FILE = 'products.csv'
 URL = 'https://services.nvd.nist.gov/rest/json/cpes/2.0'
 
-products = pd.read_csv(PRODUCTS_FILE)
+def get_cpe_names():
+    products = pd.read_csv(PRODUCTS_FILE)
 
-for index, row in products.iterrows():
-    owner = row['owner']
-    product = row['product']
-    version = row['version']
+    for index, row in products.iterrows():
+        owner = row['owner']
+        product = row['product']
+        version = row['version']
 
-    cpe_string = f'cpe:2.3:*:{owner}:{product}:{version}:*:*:*:*:*:*:*'
+        cpe_string = f'cpe:2.3:*:{owner}:{product}:{version}:*:*:*:*:*:*:*'
 
-    parameters = {
-        'cpeMatchString': cpe_string
-    }
+        parameters = {
+            'cpeMatchString': cpe_string
+        }
 
-    response = requests.get(url=URL, params=parameters)
-    # data = json.dumps(response.json()['products'])
+        response = requests.get(url=URL, params=parameters)
 
-    data = response.json()['products']
-    print(data[0]['cpe']['cpeName'])
-    
-    # with open('output.json', 'a') as file:
-    #     file.write(data)
+        data = response.json()['products']  # list of dictionaries
+
+        cpeNames = []
+
+        for i in data:
+            cpeNames.append(data[0]['cpe']['cpeName'])
+        
+        return cpeNames
+
+
+
+# get len of data
+# loop through and get the 
+
+# print(data[0]['cpe']['cpeName'])
+
+# with open('output.json', 'a') as file:
+#     file.write(data)
