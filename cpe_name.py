6fe96d675c897197f9ad3a16e2eb549a23d7ef57
Matthew Lowman <mlowmanprofessional@gmail.com>
2023-02-27 11:38:32 +0900

    Use API key

--- cpe_name.py
+++ cpe_name.py
@@ -1,8 +1,11 @@
 import requests
 import pandas as pd
+import os
 
+API_KEY = os.getenv('NVD_API_KEY')
 PRODUCTS_FILE = 'products.csv'
 URL = 'https://services.nvd.nist.gov/rest/json/cpes/2.0'
+HEADERS = {'apiKey': API_KEY}
 
 def get_cpe_names():
     products = pd.read_csv(PRODUCTS_FILE)
@@ -21,7 +24,7 @@ def get_cpe_names():
             'cpeMatchString': cpe_string
         }
 
-        response = requests.get(url=URL, params=parameters)
+        response = requests.get(url=URL, params=parameters, headers=HEADERS)
         response.raise_for_status()
         data = response.json()['products']  # list of dictionaries
 
